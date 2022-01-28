0bf5755c1ef9b51d74906a6b79ed279d102ab543
Freddie <pwed@users.noreply.github.com>
2022-01-28 20:57:45 +1100

    Initial commit

---
+++ test/bastion.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Bastion from '../lib/bastion-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/bastion-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Bastion.BastionStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
